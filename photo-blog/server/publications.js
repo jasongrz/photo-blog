c7387685b3ad5405ec0624e9a9f3a225c3cf42f6
Jason Garza <jasonmgarza47@gmail.com>
2016-12-14 23:14:22 -0600

    Add files via upload

---
+++ photo-blog/server/publications.js
@@ -0,0 +1,14 @@
+Meteor.publish('images', function(limit, userSlug) {
+  check(limit, Number);
+
+  var findQuery = {};
+  if (userSlug) {
+    check(userSlug, String);
+    findQuery = { userSlug : userSlug };
+  }
+
+  return Images.find(findQuery, {
+    limit: limit,
+    sort: { uploadedAt: -1 }
+  });
+});
